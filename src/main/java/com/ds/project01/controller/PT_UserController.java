3197b996fbb9bc0b45241dd5f20784e1ccc58644
kboo333 <kboo333@naver.com>
2023-04-28 17:55:41 +0900

    20230428_끝?

--- src/main/java/com/ds/project01/controller/PT_UserController.java
+++ src/main/java/com/ds/project01/controller/PT_UserController.java
@@ -1,62 +1,75 @@
 package com.ds.project01.controller;
 
 import java.util.HashMap;
-import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.ds.project01.dto.DeptDto;
 import com.ds.project01.dto.HobbyDataDto;
-import com.ds.project01.dto.HobbyDto;
 import com.ds.project01.dto.UserDto;
+import com.ds.project01.service.PT_UserService;
 
 @Controller
-public class UserController {
-
-	@GetMapping("/pt/write") //회원가입페이지
-	public String pt_write() {
-		
-		return "redirect:http://localhost:8081/bt/write";
-	}
+public class PT_UserController {
 	
-	@GetMapping("/user/write") // BT에서 받은 부서종류 취미종류 데이터로 가입페이지 출력
-	public String user_write(Model model, DeptDto deptList, HobbyDto hobbyList) {
-		model.addAttribute("deptList",deptList);
-		model.addAttribute("hobbyList",hobbyList);
+	@Autowired
+	PT_UserService service;
+
+	@GetMapping("/user/write")
+	public String user_write(Model model) {
+		
+		model.addAttribute("deptList", service.deptList());
+		model.addAttribute("hobbyList", service.hobbyList());
 		return "user/write";
 	}
-	
-	
-	@PostMapping("/pt/save")
+
+
+	@PostMapping("/user/save")
 	public String pt_save(UserDto dto, HobbyDataDto hdDto) {
+		service.insert(dto);
+		service.hobbyDataInsert(hdDto);
 		
 		return "redirect:/user/write";
 	}
 
 	@GetMapping("/admin/list")
-	public String admin_list(UserDto dto, Model model, String searchKeyword) {
+	public String admin_list(Model model, String searchKeyword) {
+		model.addAttribute("adminList", service.adminList(searchKeyword));
+		model.addAttribute("deptList", service.deptList());
+		model.addAttribute("hobbyList", service.hobbyList());
 		
 		return "admin/list";
 	}
 	
-	//검색한 리스트 화면을 고정한 상태로 view를 보이고 싶었음. 그래서 비동기인 ajax를 사용하기 위해 json형태로 데이터를 받으니 @ResponseBody
+	
 	@ResponseBody
 	@GetMapping("/admin/view")
-	HashMap<String, Object> userView(UserDto userDto, Model model){ //ajax로 보낸  userID 정보를 userDto에 담아서 받아옴 
+	HashMap<String, Object> userView(UserDto userDto){ //ajax로 보낸  userID 정보를 userDto에 담아서 받아옴 
 		HashMap<String, Object> map = new HashMap<>(); //attribute로 html 태그 안으로 데이터를 넘길 때는 Model, script 태그 안에는 map
+		
+		String userId =userDto.getUserId();
+		for (int i = 0; i < service.HobbyDataView(userId).size(); i++) {
+			map.put("userHobbyChoice"+i, service.HobbyDataView(userId).get(i).getHobbyEntity().getHobbyCd()); //각각 여러 취미데이터를 다른 아이디로 저장
+		}
+		map.put("deptList", service.deptList()); //받아온 부서 데이터 저장 후 script에서 불러낼것
+		map.put("getUerId",service.view(userId).getUserId());
+		map.put("getUserNm",service.view(userId).getUserNm());
+		map.put("getUserEmlAddr",service.view(userId).getUserEmlAddr());
+		map.put("getUserTelno",service.view(userId).getUserTelno());
+		map.put("getUserDeptNo",service.view(userId).getDeptEntity().getDeptNo());
+		map.put("getUserAprvYn",service.view(userId).getUserAprvYn());
 		
 		return map;
 	}
 	
 	@PostMapping("/admin/delete")
 	public String user_delete(UserDto dto) {
+		service.delete(dto);
 		return "redirect:admin/list";
 	}
 	
