f6ff662212dc79c99d65e2e3e63ca4f01165a52e
gotdori <kboo333@naver.com>
2023-06-22 00:35:52 +0900

    fix: 포트번호 변경

--- src/main/java/com/ds/project01/config/SecurityConfig.java
+++ src/main/java/com/ds/project01/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig{
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http.csrf().disable().cors().disable()
                 .authorizeRequests(request -> request
                         .antMatchers("/admin").hasRole("ADMIN") // /admin/** 경로에 대한 인증 필요
                         .anyRequest().permitAll() // 그 외의 요청은 모두 허용
